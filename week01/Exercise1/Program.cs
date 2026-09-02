b05e402db90db0588b2b1f837627797c68d66f8b
Laplace001 <steadystatesolution@gmail.com>
2026-09-02 14:16:10 +0100

    Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What's your first name?.");
+        string firstName = Console.ReadLine();
+         Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName}.");
     }
 }
+
+    
