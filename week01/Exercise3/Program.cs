a5197bc5ad1e347e63240cef463fb69be6ed3ec8
Laplace001 <steadystatesolution@gmail.com>
2026-09-03 13:52:15 +0100

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // This section generates a random number from 1 to 100
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        int guess = -1;
+
+        // loop runs until the the appropriate magic number is guessed rightly
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
+
